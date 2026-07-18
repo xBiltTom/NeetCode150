e340f6bef1f2c135bf92bfece0b692dc700c797d
xBiltTom <biltonjr@hotmail.com>
2026-07-18 16:25:44 -0500

    feat: Ejercicio twoSum en python

---
+++ Python/arrays-and-hashing/two_sum.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i] + nums[j] == target):
+                    return [i,j];
+    
+nums = [2,2,4,15]
+indexes = Solution().twoSum(nums,4)
+print(indexes)
